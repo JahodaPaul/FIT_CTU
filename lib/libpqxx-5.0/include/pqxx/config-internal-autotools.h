9c5fd31d060370e5f07bb840ff1214cac5a0689b
Pavel Jahoda <pavel.jahoda3@seznam.cz>
2017-05-14 13:58:51 +0200

    deleted libpqxx-5.0

--- lib/libpqxx-5.0/include/pqxx/config-internal-autotools.h
+++
@@ -1,8 +0,0 @@
-/* Automatically generated from config.h: internal/autotools config. */
-#define PACKAGE "libpqxx"
-#define PACKAGE_BUGREPORT "Jeroen T. Vermeulen <jtv@xs4all.nl>"
-#define PACKAGE_NAME "libpqxx"
-#define PACKAGE_STRING "libpqxx 5.0"
-#define PACKAGE_TARNAME "libpqxx"
-#define PACKAGE_VERSION "5.0"
-#define VERSION "5.0"
