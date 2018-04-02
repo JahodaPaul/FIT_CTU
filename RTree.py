f8376236aa273c97359190dab0512e95ed7b36c7
Pavel Jahoda <pavel.jahoda3@seznam.cz>
2018-04-02 11:03:08 +0200

    R-tree - working on insertion

--- RTree.py
+++ RTree.py
@@ -1,3 +1,8 @@
+class Value:
+    def __init__(self, coordinates,index):
+        self.coordinates = coordinates #TODO hardcopy these
+        self.index = index
+
 class BoundingBox:
     def __init__(self, numberOfDimensions):
         self.MinValues = [None for i in range(numberOfDimensions)]
@@ -28,10 +33,11 @@ class BoundingBox:
 
 
 class Node:
-    def __init__(self,numberOfValuesInNode):
+    def __init__(self,numberOfValuesInNode, parent):
         self.boundingBox = BoundingBox()
         self.values = [None for i in range(numberOfValuesInNode)]
         self.children = [None for i in range(numberOfValuesInNode)]
+        self.parent = parent
         self.nOfValues = 0
         self.nOfChildren = 0
 
@@ -39,19 +45,23 @@ class RTree:
     def __init__(self,numberOfValuesInNode,numberOfDimensions):
         self.numberOfValuesInNode = numberOfValuesInNode
         self.numberOfDimensions = numberOfDimensions
-        self.root = Node(self.numberOfValuesInNode)
+        self.root = Node(self.numberOfValuesInNode, None)
         self.minimumNumberOfValuesInNode = 1 if numberOfValuesInNode == 3 else int(numberOfValuesInNode/2)
 
     def Insert(self,value):
         tmp = self.root
         while true:
             if tmp.children == 0:
+                self.PropagateUpwards(tmp, value)
                 return #TODO I AM IN LEAF
             temporaryIndex = self.FindOutToWhichBoundingBoxPointBelongs(value,tmp)
             tmp = tmp.children[temporaryIndex]
         pass
 
     def Delete(self):
+        pass
+
+    def SearchForPoint(self):
         pass
 
     def SearchKClosest(self):
@@ -67,4 +77,38 @@ class RTree:
         # If not, which bounding box would be expanded least
 
         # return index
+        pass
+
+    def FindRightCombination(self, oldArray, newValue, boundingBoxesOrNot):
+        # Take old array of points/bounding boxes and new point/bounding box and find out which combination
+        # that satisfies requirements has least amount of overlays
+
+        # return two new arrays (nodes)
+        pass
+
+    def PropagateUpwards(self, node, value):
+        dealingWithBoundingBoxes = False
+
+        while true:
+            if node.nOfValues < self.numberOfValuesInNode:
+                if not dealingWithBoundingBoxes:
+                    node.values[node.nOfValues] = value
+                    #TODO recalculate boundingBox of node
+                    node.nOfValues += 1
+                else:
+                    node.values[node.nOfValues] = value.boundingBox
+                    #TODO recalculate boundingBox of node
+                    node.children[node.nOfValues] = value
+                    node.nOfValues += 1
+                    node.nOfChildren += 1
+                return
+            else:
+                existingNode, newNode = self.FindRightCombination(node,value, dealingWithBoundingBoxes)
+                node = existingNode
+                value = newNode
+                if node.parent != None:
+                    node = node.parent
+                else:
+                    pass #TODO what to do when you need to split root - create new root
+                dealingWithBoundingBoxes = True
         pass
