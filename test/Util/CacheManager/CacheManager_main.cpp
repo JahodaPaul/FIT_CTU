985809d26b4b05118122b7d3d2e5d60debcdd24d
Petr Pondělík <pondepe1@fit.cvut.cz>
2018-04-23 19:03:18 +0200

    Cache manager extension branch init commit

---
+++ test/Util/CacheManager/CacheManager_main.cpp
@@ -0,0 +1,47 @@
+#include "Util/CacheManager.hpp"
+
+using namespace RG;
+
+int main(void){
+
+    CacheManager cacheManager;
+
+    std::pair<std::string, std::shared_ptr<sf::Texture>> txtr;
+    std::pair<std::string, std::shared_ptr<sf::Texture>> txtr2;
+    std::pair<std::string, std::shared_ptr<sf::Texture>> txtr3;
+    std::pair<std::string, std::shared_ptr<sf::Texture>> txtr4;
+
+    txtr = cacheManager.getTexture("bg.png");
+    txtr2 = cacheManager.getTexture("bg.png");
+    txtr3 = cacheManager.getTexture("bg.png");
+    txtr4 = cacheManager.getTexture("bg.png");
+
+    cacheManager.deleteTexture("bg.png");
+    cacheManager.deleteTexture("bg.png");
+    cacheManager.deleteTexture("bg.png");
+    cacheManager.deleteTexture("bg.png");
+
+    sf::RenderWindow window(sf::VideoMode(800,600), "Test Window", sf::Style::Default, sf::ContextSettings{ 0u, 0u, 4u, 1u, 1u, 0u, false });
+
+    sf::RenderTexture failTextureRender;
+    failTextureRender.create(800,600);
+    failTextureRender.clear(sf::Color::Cyan);
+
+    sf::Texture failTexture = failTextureRender.getTexture();
+
+    sf::Event event;
+    while(window.isOpen()){
+        sf::Sprite sprite(*txtr.second);
+        window.draw(sprite);
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+        window.display();
+        window.clear();
+    }
+
+    return 0;
+}
