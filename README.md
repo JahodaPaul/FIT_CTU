3d3d34f8dc9fff23ccdb325ed6038db7a6c00465
Vojtěch Pejša <vojtechpejsa7@gmail.com>
2018-04-27 17:12:48 +0200

    Create better setup script.

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ RG - project for BI-SI1/BI-SP1
 ========
 
 Podrobnosti k prvnímu odevzdání ve složce 1.iterace.
+
+BUILD
+===
+Projekt potřebuje k úspěšné kompilaci staženou knihovnu sfml a box2d
+- ubuntu
+    - libsfml-dev 
+    - libbox2d-dev 
+
+Pro následné stažení grafických souborů a dalších dodatečných souborů spuste soubor 'setup.sh'
--- setup.sh
+++ setup.sh
@@ -1,6 +1,118 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Cloning imgui..."
-git clone https://github.com/ocornut/imgui.git
-echo "Cloning imgui SFML binding..."
-git clone https://github.com/eliasdaler/imgui-sfml.git
+SetupLibraries() {
+    echo ""
+    echo ""
+    echo -e "\t\tLibraries setup"
+    echo ""
+    if [ -d 'imgui' -o -d 'imgui-sfml' ]; then
+        echo "'imgui' or 'imgui-sfml' directory found, should I delete it and create new one? [y/n]"
+        read -s -n 1 ans; echo "$ans"
+        if [ "$ans" == "n" ]; then
+            return 0
+        else
+            rm -rf 'imgui' 'imgui-sfml'
+        fi fi
+    echo ""
+    echo ""
+    echo "Cloning imgui..."
+    echo ""
+    git clone https://github.com/ocornut/imgui.git
+
+    echo ""
+    echo ""
+    echo "Cloning imgui SFML binding..."
+    echo ""
+    git clone https://github.com/eliasdaler/imgui-sfml.git
+
+    echo ""
+    echo ""
+    echo "Updating imgui code..."
+    echo ""
+    cat 'imgui-sfml/imconfig-SFML.h' >> 'imgui/imconfig.h'
+    return 0
+}
+
+DownloadAssets() {
+    echo ""
+    echo ""
+    echo -e "\t\tAssets setup"
+    echo ""
+    if [ -d 'assets' ]; then
+        echo "'assets' directory found, should I delete it and create new one? [y/n]"
+        read -s -n 1 ans; echo "$ans"
+        if [ "$ans" == "n" ]; then
+            return 0
+        else
+            rm -rf 'assets'
+        fi
+    fi
+    echo ""
+    echo ""
+    echo "Creating 'assets' directory..."
+    echo ""
+    mkdir 'assets'
+
+    echo ""
+    echo ""
+    echo "Downloading assets..."
+    echo ""
+    cd 'assets'
+    wget 'https://drive.google.com/a/fit.cvut.cz/uc?authuser=1&id=1Xso4wAcikCYRvmwl9ZwqfqjggCJG3d0Z&export=download' -O 'graphics.zip' 2>/dev/null
+    unzip 'graphics.zip'
+    rm 'graphics.zip'
+    cd ..
+    return 0
+}
+
+SetupBuildDir() {
+    echo ""
+    echo ""
+    echo -e "\t\tBuild directory setup"
+    echo ""
+    if [ -d 'build' ]; then
+        echo "'build' directory found, should I delete it and create new one? [y/n]"
+        read -s -n 1 ans; echo "$ans"
+        if [ "$ans" == "n" ]; then
+            return 0
+        else
+            rm -rf 'build'
+        fi
+    fi
+
+    echo "Choose build type [1/2]:"
+    echo -e "\t1) Debug ( recomended )"
+    echo -e "\t2) Release"
+    read -s -n 1 ans; echo "$ans"
+    if [ "$ans" == "2" ]; then
+        TYPE='Release'
+    else
+        TYPE='Debug'
+    fi
+    echo "Build type chosen: ${TYPE}"
+    mkdir 'build'
+    cd 'build'
+    cmake -DCMAKE_BUILD_TYPE="${TYPE}" ..
+    ret=$?
+    cd ..
+    return $ret
+}
+
+PostSetup() {
+    echo ""
+    echo ""
+    echo -e "\t\tSetup finished, now you can build game."
+    echo ""
+    echo "To build game use:"
+    echo ""
+    echo "cd build"
+    echo "make game"
+    echo ""
+    echo "Or 'make' to make it with doxygen documntation"
+}
+
+clear
+SetupLibraries
+DownloadAssets
+SetupBuildDir
+PostSetup
