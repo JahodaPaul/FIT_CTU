88eb4bcf879356abc48c20f33fbc4f55556f2a95
Vanda Hendrychová <hendrvan@fit.cvut.cz>
2018-05-05 14:34:09 +0200

    Removed RG::Model::GameWorld class and moved its functionality to the RG::Model::Model class

--- include/Model/Model.hpp
+++ include/Model/Model.hpp
@@ -1,17 +1,18 @@
 #pragma once
 
 #include <utility> // std::pair
-#include <vector>
+#include <vector>  // std::vector
 
-#include "Model/GameWorld.hpp"
-#include "Model/Room.hpp"
+#include "Model/Floor.hpp"  // RG::Model::Floor
+#include "Model/Player.hpp" // RG::Model::Player
+#include "Model/Room.hpp"   // RG::Model::Room
 
 namespace RG {
   namespace Model {
     /**
      * \class Model
-     * \brief this class wraps up all the logic in the game and provides an interface for other layers - View and
-     * Controller
+     * \brief this class wraps up all the logic in the game and provides an
+     * interface for other layers - View and Controller
      */
     class Model {
       public:
@@ -25,28 +26,38 @@ namespace RG {
         void Move();
 
         /// returns reference to the current room
-        const RG::Model::Room& GetCurrentRoom ( void ) const;
+        const RG::Model::Room& GetCurrentRoom(void) const;
 
         /// returns player's current position
-        std::pair<float, float> GetPlayerPosition ( void ) const; // TODO 
+        std::pair<float, float> GetPlayerPosition(void) const; // TODO
 
         /// returns player's current rotation in degrees // FIXME(vanda, units)
-        float GetPlayerRotation ( void ) const;
+        float GetPlayerRotation(void) const;
 
         /// returns player's radius
-        float GetPlayerRadius ( void ) const;
+        float GetPlayerRadius(void) const;
 
-//        b2Vec GetPlayerVector ( void ) const;
+        /// returns how deep under ground the player currently is
+        int GetFloorLevel(void) const;
 
-        int GetFloorLevel ( void ) const;
+        /// returns a unique room identifier
+        int GetRoomId(void) const;
 
-        int GetRoomId (void) const;
+        std::vector<bool> GetRoomDoors(int) const;
 
-        std::vector<bool> GetRoomDoors (int) const;
-
+        // std::vector<type> GetEntityList(void) const;
       private:
+        void GenerateFloors(unsigned int seed);
+
         /// current game world
         RG::Model::GameWorld m_GameWorld;
+
+        /// an instance of the Player
+        std::shared_ptr<RG::Model::Player> m_Player;
+
+        unsigned int m_CurrentFloorIdx;
+
+        std::vector<RG::Model::Floor> m_Floors;
     };
   }
 }
--- src/Model/Model.cpp
+++ src/Model/Model.cpp
@@ -2,44 +2,56 @@
 
 namespace RG {
   namespace Model {
-    Model::Model():m_GameWorld(GameWorld("Novy Svet")){
+    Model::Model()
+      : m_CurrentFloorIdx(0)
+    {
+      m_Floors.insert(RG::Model::Floor(0, 2)); // FIXME(vanda, replace by world generation)
+      m_World = std::make_shared<b2World>(b2Vec2{ 0.0f, 0.0f });
+      m_Player = std::make_shared<RG::Model::Player>(0, 0, m_World, "Hrac");
     }
 
-    Model::~Model(){}
+    Model::~Model() {}
 
-    void Move(){}
+    void Move() {}
 
-    const RG::Model::Room& Model::GetCurrentRoom ( void ) const {
-      return RG::Model::Room(0,0); // FIXME (vanda)
+    const RG::Model::Room& Model::GetCurrentRoom(void) const
+    {
+      return RG::Model::Room(0, 0); // FIXME (vanda)
     }
 
-    std::pair<float, float> Model::GetPlayerPosition ( void ) const {
-      return {0,0}; // FIXME (vanda)
+    std::pair<float, float> Model::GetPlayerPosition(void) const
+    {
+      return { 0, 0 }; // FIXME (vanda)
     }
 
-    float Model::GetPlayerRotation ( void ) const {
+    float Model::GetPlayerRotation(void) const
+    {
       return 0; // FIXME (vanda)
     }
 
-    float Model::GetPlayerRadius ( void ) const {
+    float Model::GetPlayerRadius(void) const
+    {
       return 0; // FIXME (vanda)
     }
 
-      int Model::GetRoomId (void) const{
-          return 0; //TODO (vanda)
-      }
+    int Model::GetRoomId(void) const
+    {
+      return 0; // TODO (vanda)
+    }
 
-      int Model::GetFloorLevel ( void ) const{
-          return 0; //TODO (vanda)
-      }
+    int Model::GetFloorLevel(void) const
+    {
+      return 0; // TODO (vanda)
+    }
 
-      std::vector<bool> Model::GetRoomDoors (int floorID) const{//TODO FIXME (vanda)
-          //dummy return
-          std::vector<bool> dummy;
-          for(int i = 0;i<4;i++){
-              dummy.push_back(true);
-          }
-          return dummy;
+    std::vector<bool> Model::GetRoomDoors(int floorID) const
+    { // TODO FIXME (vanda)
+      // dummy return
+      std::vector<bool> dummy;
+      for (int i = 0; i < 4; i++) {
+        dummy.push_back(true);
       }
+      return dummy;
+    }
   }
 }
