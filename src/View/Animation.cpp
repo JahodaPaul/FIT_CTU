c02ab844243839a972b086b8ff9856eadfa9d096
Vojtěch Pejša <vojtechpejsa7@gmail.com>
2018-05-15 18:25:13 +0200

    Fix warning.

--- src/View/Animation.cpp
+++ src/View/Animation.cpp
@@ -74,7 +74,7 @@ namespace RG {
         }
 
         sf::Vector2u Animation::getSize() const {
-            return {m_frameWidth,m_frameHeight};       
+            return {(unsigned int)m_frameWidth,(unsigned int)m_frameHeight};       
         }
     }
 }
