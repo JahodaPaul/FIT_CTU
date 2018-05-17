ab454385b843d9e6fa7a9fe703097b9c9e54a146
Vojtěch Pejša <vojtechpejsa7@gmail.com>
2018-05-17 20:38:48 +0200

    Add documentation for doxygen of Scene class.

--- include/View/Scene.hpp
+++ include/View/Scene.hpp
@@ -4,16 +4,36 @@ namespace RG {
     namespace View {
         class View;
 
+        //! \class Scene
+        /*!
+         *  \brief Scene class
+         *  Abstract ancestor af all game scenes
+         */
         class Scene {
         public:
+            //! A constructor
             Scene();
 
+            //! A destructor
             virtual ~Scene();
 
+            //! Update game scene
+            /*!
+             * \param view pinter to game View
+             * \param timeElapsed time alapsed from last update
+             */
             virtual void Update(View *view, float timeElapsed);
 
+            //! Render game scene
+            /*!
+             * \param view pinter to game View
+             */
             virtual void Render(View *view);
 
+            //! Manege player input
+            /*!
+             * \param view pinter to game View
+             */
             virtual void ManageInput(View *view);
         };
     }
