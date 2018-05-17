4e1a661342f112769f87e6637e281f95e9c947f4
Vanda Hendrychová <hendrvan@fit.cvut.cz>
2018-05-17 13:39:34 +0200

    fixed angle in model

--- src/Model/DynamicObject.cpp
+++ src/Model/DynamicObject.cpp
@@ -9,21 +9,24 @@ namespace RG {
 
     DynamicObject::~DynamicObject() {}
 
-    void DynamicObject::Move(const b2Vec2& v)
+    void DynamicObject::Move(
+        const b2Vec2& v, float linear_damping, float angular_damping)
     {
       //      m_Body->ApplyAngularImpulse(M_PI, true);
       if (v.y != 0) {
         double angle = std::atan(v.x / v.y);
-        if (v.x != 0)
-          angle += M_PI / 2;
-        m_Body->SetTransform(m_Body->GetPosition(), angle);
-      } else {
+        if(angle < 0) angle += M_PI;
+        else if(angle == 0) angle += M_PI/2;
+        if(v.y < 0) angle += M_PI;
         m_Body->SetTransform(
-            m_Body->GetPosition(), v.x > 0 ? M_PI / 2 : M_PI * 3 / 2);
+            m_Body->GetPosition(), angle);
+      } else {
+        m_Body->SetTransform(m_Body->GetPosition(), v.x > 0 ? 0 : M_PI);
       }
+
       m_Body->ApplyForce(v, m_Body->GetWorldCenter(), true);
-      m_Body->SetLinearDamping(0.04f);
-      m_Body->SetAngularDamping(2.0f);
+      m_Body->SetLinearDamping(linear_damping);
+      m_Body->SetAngularDamping(angular_damping);
     }
   }
 }
