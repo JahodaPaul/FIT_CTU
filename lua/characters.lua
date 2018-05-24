96fbe194b29480c5c25a1e40b999cf36b58fe410
Vojtěch Pejša <vojtechpejsa7@gmail.com>
2018-05-24 20:31:26 +0200

    Add configuration for characters.

---
+++ lua/characters.lua
@@ -0,0 +1,26 @@
+default = {
+    textureFile = "/usr/share/RG/assets/graphics/objects/characters/zombie.png",
+    frameWidth = 100,
+    frameHeight = 87,
+    frameCount = 1,
+    frameTime = 9999,
+    rotation = 0
+}
+
+zombie = {
+    textureFile = "/usr/share/RG/assets/graphics/objects/characters/zombie.png",
+    frameWidth = 100,
+    frameHeight = 87,
+    frameCount = 8,
+    frameTime = 40,
+    rotation = 180
+}
+
+player = {
+    textureFile = "/usr/share/RG/assets/graphics/objects/characters/player.png",
+    frameWidth = 91,
+    frameHeight = 91,
+    frameCount = 12,
+    frameTime = 40,
+    rotation = 0
+}
