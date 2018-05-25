cc879c0e2efd3c1f576f9546b54bb6a96a72612b
Vojtěch Pejša <vojtechpejsa7@gmail.com>
2018-05-26 01:09:45 +0200

    SemiFix error with not removing observers on romm_change.

--- src/Util/Observer.cpp
+++ src/Util/Observer.cpp
@@ -3,7 +3,8 @@
 namespace RG { namespace Util {
     //TODO(vojta) make it pure virtual again and unsubscribre all observers when room is changed ( probably need previous room )
     void Observer::onNotify(Subject * subject, Event event) {
-        std::string msg = std::string("!!!!!!!CALL OF VIRTUAL METHOD IN OBSERVER, EVENT: \'") + std::to_string( event ) + "\' !!!!!!!";
+        std::string msg = std::string("Call of virtual method in observer, remov, event: \'") + std::to_string( event ) + "\' removing observer.";
         mainLog.Error( msg );
+        subject->RemoveObserver( this );
     }
 } }
