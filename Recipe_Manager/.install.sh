53351a26e3fb81083749bec26886c04a5b7641a8
Pavel Jahoda <pavel.jahoda3@seznam.cz>
2018-06-01 11:05:09 +0200

    deleting libpq and curses folder from Recipe_Manager, those are now downloaded during installation

--- Recipe_Manager/.install.sh
+++ Recipe_Manager/.install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#get files from libraries libpq and curses
+wget -O ./lib/libpq.zip https://www.dropbox.com/sh/tnd0jhnh4mdcbd0/AABEqRNhapunJPZCOxmKSrqKa?dl=1
+unzip ./lib/libpq.zip -d ./lib/libpq
+
+wget -O ./lib/curses.zip https://www.dropbox.com/sh/p1e3ntdt7hkmww9/AABsHWERn4wjV7csCCSAkxqja?dl=1
+unzip ./lib/curses.zip -d ./lib/curses
+
+rm ./lib/curses.zip
+rm ./lib/libpq.zip
+
 #fedora
 fedora=$(grep "edora$" /etc/*-release | wc -l)
 centos=$(grep "entos$" /etc/*-release | wc -l)
