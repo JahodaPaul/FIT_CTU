5c8deaa193b1123d065057adf253d61906b14eb3
Jan Lidák <lidakja1@fit.cvut.cz>
2018-12-05 22:00:03 +0100

    Task 4

--- main.py
+++ main.py
@@ -1,36 +1,54 @@
+%matplotlib inline
+import matplotlib.pyplot as plt
 import numpy as np
-#import matplotlib.pyplot as plt
+import pandas as pd
+from scipy import stats
 
 dataPath = 'data.csv'
 
-def importData():
-    with open('data.csv','r') as f:
-        data = [line.rstrip('\n') for line in f]
-        survive = []
-        weights = []
-        for cnt, item in enumerate(data):
-            data[cnt] = item.split(';')
-            if cnt != 0:
-                weights.append(float(data[cnt][0]))
-                data[cnt][1] = data[cnt][1][1:-1] # the column contains words 'perished' and 'survived', for easier use
-                                                  # I will convert 'perished' -> 0 and 'survived' -> 1
-                if data[cnt][1] == 'survived':
-                    survive.append(1)
-                else:
-                    survive.append(0)
+def importData(dataPath):
+    data = pd.read_csv(dataPath, sep=';').replace({'perished' : 0, 'survived' : 1}).reset_index()
+    return np.array(data.Weight), np.array(data.Status)
 
-        weights = np.array(weights)
-        survive = np.array(survive)
-        return weights, survive
+def SolutionTask4(mi=25.793220423035702,sig=1.9175809969270372, EV=0.5932203389830508):
+#     np.random.seed(42)
+    W_gen = []
+    S_gen = []
+    
+    for i in range(0,100): #59 is sample size, so 100 values might feel a bit off
+        W_gen.append(np.random.normal(loc=mi,scale=np.sqrt(sig)))
+        S_gen.append(np.random.choice(a=[0, 1], p=[1-EV,EV]))
+    
+    bins = np.linspace(21, 32, 25)
+    plt.grid(axis='y', alpha=0.75)
+    plt.xlabel('Weight')
+    plt.ylabel('No. of birbs')
 
+    plt.hist(w_gen, bins, color='red', alpha=0.5, label='generated')
+    plt.hist(weights, bins, color='blue', alpha=0.5, label='given')
+    plt.legend(loc='upper right')
+    plt.show()
+
+    plt.xlabel('Survival')
+    plt.ylabel('No. of birbs')
+
+    plt.hist(s_gen, color='red', alpha=0.5, label='generated')
+    plt.hist(survive, color='blue', alpha=0.5, label='given')
+    plt.legend(loc='upper center')
+    plt.show()
+
+    
 def SolutionTask1(weights, survive):
+    print('Weight of birbs')
     print('Expected value:',np.mean(weights)) # assuming it's an uniform distribution
     print('Variance:',np.var(weights))
     print('Median:',np.median(weights))
-
+    print('==================================')
+    print('Survival of birbs')
     print('Expected value:', np.mean(survive))  # assuming it's an uniform distribution
     print('Variance:', np.var(survive))
     print('Median:', np.median(survive))
 
-weights, survive = importData()
+weights, survive = importData('data.csv')
 SolutionTask1(weights=weights,survive=survive)
+SolutionTask4()
