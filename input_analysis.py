8c7c22829508e7a149419576e376620a9725e74c
Pavel Jahoda <pavel.jahoda3@seznam.cz>
2019-04-04 00:18:51 +0800

    Finished hypothesis testing section of input analysis

--- input_analysis.py
+++ input_analysis.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from scipy.stats import chisquare
+from numpy.random import normal
+import math
+from scipy.stats import normaltest
+from scipy.special import erf
+
 
 class Input_analysis:
     def __init__(self):
@@ -22,6 +28,23 @@ class Input_analysis:
         for i in range(1,len(self.arrival_times)):
             self.arrivel_times_processed.append(self.arrival_times[i]-self.arrival_times[i-1])
 
+    def Scatter(self):
+        plt.scatter(self.call_durations,self.velocities)
+        plt.title('Calls',size=22)
+        plt.xlabel('Call duration (seconds)',size=18)
+        plt.ylabel('Car Velocity (km/h)',size=18)
+        plt.show()
+
+    def CheckData(self):
+        error_values = False
+        for item in self.base_stations:
+            if item < 1 or item > 20:
+                error_values = True
+        if error_values:
+            print('There are bad values in the dataset')
+        else:
+            print('No error values')
+
     def ShowHistograms(self):
         titleSize = 24
         labelSize = 20
@@ -41,7 +64,7 @@ class Input_analysis:
         plt.show()
 
         plt.hist(self.call_durations, color='skyblue')
-        plt.title('Call duratins', size=titleSize)
+        plt.title('Call durations', size=titleSize)
         plt.xlabel('seconds', size=labelSize)
         plt.ylabel('No. of occurrences', size=labelSize)
         # plt.savefig('./Report/images/survivedHist.png', bbox_inches='tight')
@@ -54,21 +77,7 @@ class Input_analysis:
         # plt.savefig('./Report/images/survivedDist.png', bbox_inches='tight')
         plt.show()
 
-        # ecdf = ECDF(weightsSurvived)
-        # plt.plot(ecdf.x, ecdf.y, color='g')
-        # plt.title('Sparrows that survived', size=titleSize)
-        # plt.ylabel('Cumulative Distribution Function', size=labelSize)
-        # plt.xlabel('Weights', size=labelSize)
-        # # plt.savefig('./Report/images/diedHist.png', bbox_inches='tight')
-        # plt.show()
-        #
-        # ecdf = ECDF(weightsDied)
-        # plt.plot(ecdf.x, ecdf.y, color='firebrick')
-        # plt.title('Sparrows that perished', size=titleSize)
-        # plt.ylabel('Cumulative Distribution Function', size=labelSize)
-        # plt.xlabel('Weights', size=labelSize)
-        # # plt.savefig('./Report/images/diedDist.png', bbox_inches='tight')
-        # plt.show()
+
     def CalculateParameters(self):
         self.lambda_interarrival = len(self.arrivel_times_processed)/float(sum(self.arrivel_times_processed))
         self.lambda_callDuration = len(self.call_durations)/float(sum(self.call_durations))
@@ -81,7 +90,60 @@ class Input_analysis:
         self.variance_velocities = self.variance_velocities/float(len(self.velocities))
         print('mean velocities:',self.mean_velocities,'variance velocities:',self.variance_velocities)
 
+    def HypothesisTesting(self):
+        print('Velocities mean:',np.mean(np.array(self.velocities)))
+        norm_vel = normal(self.mean_velocities, math.sqrt(self.variance_velocities),len(self.velocities))
+        print(normaltest(self.velocities))
+        print('Chi square test:',chisquare(self.velocities,norm_vel,2))
+
+    def CalculateChiSquareTestRanges(self):
+        self.rangesInterArrival = []
+        self.rangesCallDuration = []
+        prevInt = 0
+        prevDur = 0
+        for i in range(1,100):
+            x1 = math.log(-1*(i/100)+1,math.e)/self.lambda_interarrival*(-1)
+            x2 = math.log(-1 * (i / 100) + 1, math.e) / self.lambda_callDuration * (-1)  + min(self.call_durations)
+            self.rangesInterArrival.append((prevInt,x1))
+            self.rangesCallDuration.append((prevDur,x2))
+            prevInt = x1
+            prevDur = x2
+            print(x1,x2)
+        self.rangesInterArrival.append((prevInt,max(self.arrivel_times_processed)+1)) # bigger number than all of them..
+        self.rangesCallDuration.append((prevDur,max(self.call_durations)+1))
+
+    def CalculateFrequencies(self):
+        self.interArrivalFrequencies = [0 for i in range(100)]
+        self.durationFrequencies = [0 for i in range(100)]
+        for i in range(100):
+            for item in self.arrivel_times_processed:
+                if self.rangesInterArrival[i][0] <= item and self.rangesInterArrival[i][1] > item:
+                    self.interArrivalFrequencies[i] += 1
+
+            for item in self.call_durations:
+                if self.rangesCallDuration[i][0] <= item and self.rangesCallDuration[i][1] > item:
+                    self.durationFrequencies[i] += 1
+        print("duration call frequncies:",self.durationFrequencies)
+        print("inter-arrival times frequencies",self.interArrivalFrequencies)
+
+    def ChiSquareTest(self):
+        sumInter = 0
+        sumDur = 0
+        for i in range(100):
+            # print(sumInter,sumDur)
+            sumInter += math.pow((self.interArrivalFrequencies[i]-100),2)/100
+            sumDur += math.pow((self.durationFrequencies[i]-100),2)/100
+        print(sumInter)
+        print(sumDur)
+
+
 analysis = Input_analysis()
 analysis.ReadData()
+analysis.CheckData()
 analysis.CalculateParameters()
-analysis.ShowHistograms()
+analysis.HypothesisTesting()
+# analysis.Scatter()
+# analysis.ShowHistograms()
+analysis.CalculateChiSquareTestRanges()
+analysis.CalculateFrequencies()
+analysis.ChiSquareTest()
